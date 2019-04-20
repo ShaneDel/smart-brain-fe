c181649a0f11b43bfcded65d2ede2b82c507f1b3
ShaneDel <shanedel@gmail.com>
2019-04-19 21:01:26 -0500

    Front End app for Smart Brain

---
+++ build/precache-manifest.2860c42705a6266618dad037579a14a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb343d6745b7e0470c8f",
+    "url": "/static/js/main.2c1d6e92.chunk.js"
+  },
+  {
+    "revision": "497a27a978a7c5a053fc",
+    "url": "/static/js/2.343b017c.chunk.js"
+  },
+  {
+    "revision": "cb343d6745b7e0470c8f",
+    "url": "/static/css/main.a3e90782.chunk.css"
+  },
+  {
+    "revision": "497a27a978a7c5a053fc",
+    "url": "/static/css/2.f8196099.chunk.css"
+  },
+  {
+    "revision": "18ed3b74012f039cb1e61f635cba9c00",
+    "url": "/index.html"
+  }
+];
