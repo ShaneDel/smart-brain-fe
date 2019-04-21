b7da9a86a54a3c4e75a99707aa66162ae61b0194
ShaneDel <shanedel@gmail.com>
2019-04-20 20:51:47 -0500

    Correcting end points

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://calm-shore-22072.herokuapp.com:3000/image', {
+          fetch('https://calm-shore-22072.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
