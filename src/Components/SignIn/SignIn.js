06e02e6338c1c05f9c66bb203cc2a4eff9377e1b
ShaneDel <shanedel@gmail.com>
2019-04-20 20:20:43 -0500

    Changed end points for heroku deployment

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://calm-shore-22072.herokuapp.com/signin', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
